800ea1eaf68c454d2fd33b2b05a12d5fcfb00b30
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-10-18 23:09:19 +0530

    Adding NavBar and Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {NavLink } from 'react-router-dom'
+
 
 function Footer() {
   return (
     <footer>
-      Footer
+        <div className='links'>
+            <NavLink className="footer-link" to="/">Home</NavLink>
+            <NavLink className="footer-link" to="/about">About</NavLink>
+            <NavLink className="footer-link" to="/projects">Projects</NavLink>
+            <NavLink className="footer-link" to="/testimonials">Testimonials</NavLink>
+            <NavLink className="footer-link" to="/updates">Updates</NavLink>
+            <NavLink className="footer-link" to="/collaborations">Collaborations</NavLink>
+            <NavLink className="footer-link" to="/contact">Contact</NavLink>
+        </div>
     </footer>
   )
 }
