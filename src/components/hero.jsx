b171678a65a35a8915c1439ee9829c02c4c83757
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-10-27 22:24:38 +0530

    Added home hero

---
+++ src/components/hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import illustration from '../assets/illustration.jpg'
+
+function hero() {
+  return (
+    <div class='homehero'>
+      <img src={illustration} alt="" />
+    </div>
+  )
+}
+
+export default hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeAbout from '../components/HomeAbout'
+import Hero from '../components/hero'
 
 function Home() {
   return (
     <div>
       Home Page
+      <Hero/>
       <HomeAbout/>
     </div>
   )
