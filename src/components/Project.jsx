5a3fae45b821306c1caa01e812ecde2ed49c2229
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-09 22:25:23 +0530

    Responsive Design

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ function Project() {
                 <h1 className='text-darkgreen p-4 text-4xl font-extrabold'>
                     OUR PROJECTS
                 </h1>
-                <div id='projects' className='flex font-extrabold  flex-row p-9 leading-8 justify-around items-center'>
+                <div id='projects' className='flex font-extrabold flex-col md:flex-row p-9 md:px-[10%] leading-8 justify-around items-center'>
                     <div id='left'>
                         <h1 className='text-3xl pb-4 text-darkgreen'>Past Projects</h1>
                         <div className='text-center text-darkgreen font-extrabold bg-lightgreen p-8 rounded-3xl'>
@@ -25,6 +25,7 @@ function Project() {
                             </div>
                         </div>
                     </div>
+                    <br />
                     <div id='right'>
                         <h1 className='text-3xl pb-4 text-darkgreen'>Current Projects</h1>
                         <div className='text-center text-darkgreen font-extrabold bg-lightgreen p-8 rounded-3xl'>
@@ -42,15 +43,15 @@ function Project() {
                     </div>
                 </div>
                 <div>
-                    <button className='std-btn bg-lightgreen text-white rounded-full px-9'>Learn More</button>
+                    <button className='std-btn bg-darkgreen text-white rounded-full px-9'>Learn More</button>
                 </div>
             </div>
 
-            <div className='future-projects text-darkgreen font-extrabold flex flex-row justify-around w-full p-14'>
-                <div id='left' className='w-1/2'>
-                    <img src={image1} alt="" className='h-auto w-3/4 self-center rounded-xl' />
+            <div className='future-projects text-darkgreen font-extrabold flex flex-col md:flex-row justify-around w-full p-8'>
+                <div id='left' className='md:w-1/2'>
+                    <img src={image1} alt="" className='h-auto md:w-3/4 mb-8 self-center rounded-xl' />
                 </div>
-                <div id='right' className='w-1/2'>
+                <div id='right' className='md:w-1/2'>
                     <h1 className='font-extrabold text-3xl'>
                         FUTURE PROJECTS
                     </h1>
